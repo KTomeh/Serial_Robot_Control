ae8564e69af713858881501ad57b9a9660df4bd7
LAPTOP-KB7TEM1B\Robot <kusaytomah@gmail.com>
2025-01-28 14:55:24 +0100

    Session_3_ready

---
+++ session2.m
@@ -0,0 +1,57 @@
+%% Developed by Kusay Tomeh, kusay.tomah@gmail.com
+%% with help from matlab contents
+%% add folders to matlab pathes
+addpath('initilization');
+addpath('Images');
+addpath('Libraries');
+addpath('RobotSTLFiles');
+
+%% simscape constants
+joint_damping = 0.2;
+grip_stiffness = 1.0 ;
+grip_damping = 5.0 ;
+floor_dimensions = [60 60 0.5] ;%c.m
+gripUpperLimit = 2.0 ;%c.m
+gripLowerLimit = -2.0 ;%c.m
+
+%% Design constraints
+torqueUpperLimit = 2.0;%N.m
+torqueLowerLimit = -2.0 ;%N.m
+jointVelocityUpperLimit = 2*pi;%rad per sec
+jointVelocityLowerLimit = -2*pi;%rad per sec
+
+%% robot tree
+[robot,homeConfig] = createRigidBodyTree();
+
+%% set initial position of joints based on simulation case
+J1_initial_position= 0;%rad
+J2_initial_position= -pi/2;%rad
+J3_initial_position= -pi/2;%rad
+J4_initial_position = 0;%rad
+
+%% robot model variant selection
+RobotModelToSimulate = 0;
+% 0 <==> All joints actuation is position q
+% 5 <==> All joints actuation is Torque tau
+% 1 <==> joint1 actuation is torque tau , All other joints actuation is position q
+% 2 <==> joint2 actuation is torque tau , All other joints actuation is position q
+% 3 <==> joint3 actuation is torque tau , All other joints actuation is position q
+% 4 <==> joint4 actuation is torque tau , All other joints actuation is position q
+Robot_MotionAcuated= Simulink.Variant('RobotModelToSimulate==0');
+Robot_J1torqueActuated= Simulink.Variant('RobotModelToSimulate==1');
+Robot_J2torqueActuated= Simulink.Variant('RobotModelToSimulate==2');
+Robot_J3torqueActuated= Simulink.Variant('RobotModelToSimulate==3');
+Robot_J4torqueActuated= Simulink.Variant('RobotModelToSimulate==4');
+Robot_TorqueAcuated= Simulink.Variant('RobotModelToSimulate==5');
+
+%%
+uiopen('sessions\session2\motion_joints.slx',1);
+
+%%
+wayPoints_simple_trajectory = [0.2 -0.2 0.15; 0.2 -0.2 0.02;0.25 0 0.15; 0.2 0.2 0.02];% 4 points
+wayPoints_complex_trajectory = [0.2 -0.2 0.15; 0.2 -0.2 0.02; 0.15 0 0.28;0.15 0.05 0.2;...
+    0.15 0.09 0.15; 0.1 0.12 0.1; 0.04 0.1 0.2; 0.25 0 0.15; 0.2 0.2 0.02];% 9 points
+wayPoints_trajectory = wayPoints_simple_trajectory;
+
+%%
+uiopen('sessions\session2\motion_planning.slx',1);
--- session3.m
+++ session3.m
@@ -1,10 +1,16 @@
-%% Developed by Kusay Tomeh, kusay.tomah@gmail.com
-%% with help from matlab contents
+%% Developed by Kusay Tomeh,
+%% kusay.tomah@gmail.com  
 %% add folders to matlab pathes
 addpath('initilization');
 addpath('Images');
 addpath('Libraries');
 addpath('RobotSTLFiles');
+
+%% Design constraints
+torqueUpperLimit = 2.0;%N.m
+torqueLowerLimit = -2.0 ;%N.m
+jointVelocityUpperLimit = 2*pi;%rad per sec
+jointVelocityLowerLimit = -2*pi;%rad per sec
 
 %% simscape constants
 joint_damping = 0.2;
@@ -13,12 +19,6 @@ grip_damping = 5.0 ;
 floor_dimensions = [60 60 0.5] ;%c.m
 gripUpperLimit = 2.0 ;%c.m
 gripLowerLimit = -2.0 ;%c.m
-
-%% Design constraints
-torqueUpperLimit = 2.0;%N.m
-torqueLowerLimit = -2.0 ;%N.m
-jointVelocityUpperLimit = 2*pi;%rad per sec
-jointVelocityLowerLimit = -2*pi;%rad per sec
 
 %% robot tree
 [robot,homeConfig] = createRigidBodyTree();
@@ -30,7 +30,7 @@ J3_initial_position= -pi/2;%rad
 J4_initial_position = 0;%rad
 
 %% robot model variant selection
-RobotModelToSimulate = 0;
+RobotModelToSimulate = 1;
 % 0 <==> All joints actuation is position q
 % 5 <==> All joints actuation is Torque tau
 % 1 <==> joint1 actuation is torque tau , All other joints actuation is position q
@@ -43,15 +43,24 @@ Robot_J2torqueActuated= Simulink.Variant('RobotModelToSimulate==2');
 Robot_J3torqueActuated= Simulink.Variant('RobotModelToSimulate==3');
 Robot_J4torqueActuated= Simulink.Variant('RobotModelToSimulate==4');
 Robot_TorqueAcuated= Simulink.Variant('RobotModelToSimulate==5');
+return
 
-%%
-uiopen('sessions\session1\motion_joints.slx',1);
-
-%%
-wayPoints_simple_trajectory = [0.2 -0.2 0.15; 0.2 -0.2 0.02;0.25 0 0.15; 0.2 0.2 0.02];% 4 points
-wayPoints_complex_trajectory = [0.2 -0.2 0.15; 0.2 -0.2 0.02; 0.15 0 0.28;0.15 0.05 0.2;...
-    0.15 0.09 0.15; 0.1 0.12 0.1; 0.04 0.1 0.2; 0.25 0 0.15; 0.2 0.2 0.02];% 9 points
-wayPoints_trajectory = wayPoints_simple_trajectory;
-
-%%
-uiopen('sessions\session1\motion_planning.slx',1);
+%% Please run the follwing four models individually
+%% set joint 1 as troque actuated , All other joints actuation is position q
+RobotModelToSimulate = 1;
+uiopen('sessions\session3\reduced_model_PT1_J1.slx',1);
+return
+%% set joint 2 as troque actuated , All other joints actuation is position q
+RobotModelToSimulate = 2;
+uiopen('sessions\session3\reduced_model_PT1_J2.slx',1);
+return
+%% set joint 3 as troque actuated , All other joints actuation is position q
+RobotModelToSimulate = 3;
+uiopen('sessions\session3\reduced_model_PT1_J3.slx',1);
+return
+%% set joint 4 as troque actuated , All other joints actuation is position q
+RobotModelToSimulate = 4;
+J2_initial_position= 0;%rad
+J3_initial_position= 0;%rad
+uiopen('sessions\session3\reduced_model_PT1_J4.slx',1);
+return
